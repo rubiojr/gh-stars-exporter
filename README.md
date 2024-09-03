934d6cc9bf883c99c3fc97548ad39ea8490cf598
Sergio Rubio <rubiojr@rbel.co>
2024-09-03 20:54:41 +0200

    Add JSON exports support and offline mode

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # gh-stars-exporter
 
-Export GitHub stars to a SQLite database.
+Export GitHub stars to a SQLite database and JSON.
 
 ## Install
 
@@ -14,3 +14,12 @@ go install github.com/kevinpollet/gh-stars-exporter@latest
 export GITHUB_TOKEN=your_github_token
 gh-stars-exporter --db stars.db
 ```
+
+### JSON exports
+
+```bash
+export GITHUB_TOKEN=your_github_token
+gh-stars-exporter --db stars.db --json > ghstars.json
+```
+
+JSON exports also supports offline mode, using the `--skip-update` flag. It'll export the stars from the existing database.
--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"net/url"
 	"os"
 	"strings"
 	"time"
@@ -48,68 +49,109 @@ var logger = log.NewWithOptions(os.Stderr, log.Options{
 	ReportTimestamp: false,
 })
 
-func main() {
-	flag.Parse()
+func jsonExport(sess db.Session) error {
+	stars := []*Repository{}
+	sess.Collection("starred_repos").Find().All(&stars)
 
-	token := os.Getenv("GITHUB_TOKEN")
-	if token == "" {
-		logger.Fatal("GITHUB_TOKEN is required")
+	b, err := json.MarshalIndent(stars, "", "  ")
+	if err != nil {
+		return err
 	}
+	fmt.Println(string(b))
 
-	if debug {
-		logger.SetLevel(log.DebugLevel)
-	}
+	return nil
+}
 
+func dbInit() (db.Session, error) {
 	if err := migrateDB(); err != nil {
-		logger.Fatal(err)
+		return nil, err
 	}
 
 	settings := sqlite.ConnectionURL{
 		Database: dbFile,
 	}
 	sess, err := sqlite.Open(settings)
-	if err != nil {
-		logger.Fatal(err)
+
+	return sess, err
+}
+
+func token() string {
+	token := os.Getenv("GITHUB_TOKEN")
+	if token == "" {
+		logger.Fatal("GITHUB_TOKEN is required")
 	}
-	defer sess.Close()
+	return token
+}
 
-	stars := sess.Collection("starred_repos")
-	err = fetchAllStarredRepos(os.Getenv("GITHUB_TOKEN"), func(repos []Repository) error {
+func main() {
+	flag.Parse()
 
-		for _, repo := range repos {
-			res := stars.Find(db.Cond{"id": repo.ID})
-			ok, err := res.Exists()
-			if ok || err != nil {
-				continue
-			}
+	if debug {
+		logger.SetLevel(log.DebugLevel)
+	}
 
-			repo.Topics = strings.Join(repo.TopicList, ",")
-			if repo.Private && !storePrivate {
-				logger.Infof("skipping private repository %s", repo.FullName)
-				continue
-			}
-
-			_, err = sess.Collection("starred_repos").Insert(repo)
-			if err != nil {
-				return nil
-			}
+	if skipUpdate && jsonFlag {
+		if _, err := os.Stat(dbFile); os.IsNotExist(err) {
+			logger.Fatal("Database file not found, use the exporter without --skip-update at least once.")
 		}
+	}
 
-		return nil
-	})
-
+	sess, err := dbInit()
 	if err != nil {
-		logger.Fatal(err)
+		logger.Fatal("opening database", err)
+	}
+	stars := sess.Collection("starred_repos")
+
+	updatedStars := 0
+	if !skipUpdate {
+		logger.Info("Fetching stars from github.com...")
+		err = fetchAllStarredRepos(token(), func(repos []Repository) error {
+			for _, repo := range repos {
+				repo.Topics = strings.Join(repo.TopicList, ",")
+				if repo.Private && !storePrivate {
+					logger.Warnf("Skipping private repository %s", repo.FullName)
+					continue
+				}
+
+				res := stars.Find(db.Cond{"id": repo.ID})
+				ok, err := res.Exists()
+				if ok || err != nil {
+					return nil
+				}
+
+				updatedStars++
+				_, err = sess.Collection("starred_repos").Insert(repo)
+				if err != nil {
+					return err
+				}
+			}
+
+			return nil
+		})
+
+		logger.Infof("Updated stars: %d", updatedStars)
+	} else {
+		logger.Info("Skipping update (offline mode)")
+	}
+
+	if jsonFlag {
+		err = jsonExport(sess)
+		if err != nil {
+			logger.Fatal("exporting to JSON", err)
+		}
 	}
 }
 
 func fetchAllStarredRepos(githubToken string, iterator func([]Repository) error) error {
 	nextPageURL := "https://api.github.com/user/starred?per_page=100"
 
-	client := &http.Client{}
+	client := &http.Client{
+		Timeout: time.Second * 10,
+	}
 
+	currentPage := 1
 	for nextPageURL != "" {
-		logger.Debugf("Fetching stars %s", nextPageURL)
+		logger.Debugf("Page URL %s", nextPageURL)
 		req, err := http.NewRequest("GET", nextPageURL, nil)
 		if err != nil {
 			return err
@@ -135,7 +177,14 @@ func fetchAllStarredRepos(githubToken string, iterator func([]Repository) error)
 			return err
 		}
 
-		nextPageURL = getNextPageURL(resp.Header.Get("Link"))
+		pagerLink := resp.Header.Get("Link")
+		nextPageURL = getNextPageURL(pagerLink)
+		pageCount := getPageCount(pagerLink)
+		if pageCount == "" {
+			pageCount = fmt.Sprintf("%d", currentPage)
+		}
+		logger.Debugf("Fetching stars... (page %d/%s)", currentPage, pageCount)
+		currentPage++
 	}
 
 	return nil
@@ -153,6 +202,28 @@ func getNextPageURL(linkHeader string) string {
 		if len(parts) == 2 && strings.TrimSpace(parts[1]) == `rel="next"` {
 			nextPageURL := strings.TrimSpace(parts[0])
 			return strings.Trim(nextPageURL, "<>")
+		}
+	}
+
+	return ""
+}
+
+// getNextPageURL parses the Link header from GitHub API response and finds the URL for the next page.
+func getPageCount(linkHeader string) string {
+	if linkHeader == "" {
+		return ""
+	}
+
+	links := strings.Split(linkHeader, ",")
+	for _, link := range links {
+		parts := strings.Split(link, ";")
+		if len(parts) == 2 && strings.TrimSpace(parts[1]) == `rel="last"` {
+			nextPageURL := strings.TrimSpace(parts[0])
+			u, err := url.Parse(strings.Trim(nextPageURL, "<>"))
+			if err != nil {
+				return ""
+			}
+			return u.Query().Get("page")
 		}
 	}
 
@@ -180,10 +251,14 @@ func migrateDB() error {
 
 var dbFile string
 var debug bool
+var jsonFlag bool
 var storePrivate bool
+var skipUpdate bool
 
 func init() {
 	flag.StringVar(&dbFile, "db", "data.ghstars", "Database file")
 	flag.BoolVar(&debug, "debug", false, "Enable debug mode")
+	flag.BoolVar(&skipUpdate, "skip-update", false, "Do not update the database (offline, use existing data)")
+	flag.BoolVar(&jsonFlag, "json", false, "JSON Export to stdout")
 	flag.BoolVar(&storePrivate, "store-private", false, "Store private starred repositories")
 }
